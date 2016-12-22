d8a598148d95e7ea9bec11f1ba40c9d8bdb661ee
Keith Damiani <keith.damiani@gmail.com>
2016-12-22 14:57:46 -0500

    Fix post link on category page

--- source/_layouts/category.blade.php
+++ source/_layouts/category.blade.php
@@ -26,7 +26,7 @@
 @foreach ($posts->where('category', $filename) as $post)
 <div class="row">
     <div class="col-xs-12">
-        <h3><a href="{{ $url }}">{{ $post['title'] }}</a></h3>
+        <h3><a href="{{ $post->url }}">{{ $post['title'] }}</a></h3>
         <p class="text-sm">by {{ $post->author }} · {{ $post->date_formatted() }} · Number {{ $post->number }}</p>
         <p class="p-xs-b-6 border-b">{!! $post->preview(180) !!}...</p>
     </div>
